72ae1a4de69f6e869ffb0b125055079b7016fbd8
Steven Mosquera <129177391+smosquerap@users.noreply.github.com>
2023-04-13 10:12:22 -0500

    feat: issues component

---
+++ src/issues/components/filter-selector/StateSelector.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { useIssuesStore } from 'src/stores/issues';
+
+const issueStore = useIssuesStore();
+</script>
+
+<template>
+  <q-btn-toggle
+    toggle-color="primary"
+    push
+    spread
+    :options="[
+      {
+        label: 'All',
+        value: '',
+      },
+      {
+        label: 'Open',
+        value: 'open',
+      },
+      {
+        label: 'Closed',
+        value: 'closed',
+      },
+    ]"
+    v-model="issueStore.state"
+  />
+</template>
+
+<style scoped></style>
---
+++ src/issues/pages/IssuePage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import LoaderSpinner from 'src/shared/components/LoaderSpinner.vue';
+import IssueCard from '../components/issue-list/IssueCard.vue';
+
+const route = useRoute();
+const { id } = route.params;
+</script>
+
+<template>
+  <LoaderSpinner />
+  <router-link class="text-white" to="/issues">Go back</router-link>
+  <IssueCard />
+  <LoaderSpinner :thickness="2" size="1.5rem" :show-text="false" />
+  <div class="column">
+    <span class="text-h3 q-mb-md">Comments (4)</span>
+    <IssueCard v-for="comment of 4" :key="comment" />
+  </div>
+</template>
+
+<style scoped></style>
